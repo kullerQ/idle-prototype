44438e9de6c3847619dcb5165fe627c85ddb6341
kullerQ <60192357+kullerQ@users.noreply.github.com>
2026-07-20 16:24:02 +0200

    final refactor - phase 5c

--- features/expeditions/expedition_ui/expedition_ui.gd
+++ features/expeditions/expedition_ui/expedition_ui.gd
@@ -9,12 +9,16 @@ var manager: ExpeditionManager
 func _ready() -> void:
 	manager.hp_changed.connect(_on_hp_changed)
 	manager.expedition_started.connect(_on_expedition_started)
+	progress_bar_time_left.hide()
 
 
 func _on_expedition_started() -> void:
-	push_warning("WIP: Not implemented yet: expedition_started")
-#	progress_bar_hp.
+	progress_bar_hp.value = 1.0
+	progress_bar_time_left.value = 1.0
 
 
 func _on_hp_changed(new_hp: float, max_hp: float) -> void:
-	progress_bar_hp.value = new_hp / max_hp 
+	if max_hp <= 0.0:
+		progress_bar_hp.value = 0.0
+		return
+	progress_bar_hp.value = new_hp / max_hp
